3784f5f651d8330761c2242f66c85fa6f00c1f42
kaye-sub <kwonk0214@gmail.com>
2024-12-12 14:15:02 +0900

    🍱 Dockerfile 추가

--- src/main/kotlin/kr/ac/kumoh/ce/s20240058/w24backend/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20240058/w24backend/controller/MessageController.kt
@@ -2,14 +2,14 @@ package kr.ac.kumoh.ce.s20240058.w24backend.controller
 
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/message")
+@CrossOrigin(origins= ["http://localhost:3000", "https://kaye-w24frontend.netlify.app/"])
 class MessageController {
     @GetMapping
-    // TODO: React 주소 추가
-    @CrossOrigin(origins= ["http:localhost:3000"])
+    fun getRoot() = mapOf("status" to "success")
+
+    @GetMapping("/api/message")
     fun getMessage() = mapOf("status" to "success")
 }
